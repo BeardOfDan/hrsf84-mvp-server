4d00ac4bee497ad8259337288d10454fb1b0d554
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-11 13:41:32 -0800

    Refactor 404 response to be a string

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get('*', /*Conditionally Authenticate User Here*/(req, res, next) => {
         // res.status(404).redirect('/html/404Page.html');
         res.status(404)
           .append('Access-Control-Allow-Origin', ['*'])
-          .end({ 'error': 'The url "' + storyName + '" does not exist!' });
+          .end(JSON.stringify({ 'error': 'The url "' + storyName + '" does not exist!' }));
       }
     });
 
